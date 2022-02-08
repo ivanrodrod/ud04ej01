e3f73e8db5e6bbff99429e70526d32d122938ca2
ivanrodrod <80984814+ivanrodrod@users.noreply.github.com>
2022-02-08 01:10:55 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 4; index++) {
         basic.showIcon(IconNames.Heart)
-        basic.pause(500)
+        basic.pause(100)
         basic.showIcon(IconNames.SmallHeart)
     }
     basic.showIcon(IconNames.StickFigure)
